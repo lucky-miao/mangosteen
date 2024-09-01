cb20befb4dd405f75247cc7e41ff77618c8259ef
miaoyunqi <810790131@qq.com>
2024-09-01 19:07:27 +0800

    引入 kaminari

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_30_184810) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_01_100629) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "amount"
+    t.text "note"
+    t.bigint "tags_id", array: true
+    t.datetime "happen_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
